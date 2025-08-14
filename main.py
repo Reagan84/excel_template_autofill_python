2877c93f852f1919177085d3bbfc2f1b22e9707d
cnel7 <carzonn216@gmail.com>
2025-08-13 20:25:10 -0600

    implementing very basics of openpyxl

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # TODO:
 # have program open, from a file path, excel
+from openpyxl import Workbook, load_workbook
 
-print("Provided me with HTML file path")
-fpath = input()
-print(f"Here: {fpath}")
+wb = Workbook()
+
+fpath = "../../../Desktop/"
+
+print("Provided me with a file")
+fname = input()
+ftotal = fpath + fname
+xlTemplate = load_workbook(filename = ftotal)
+
+
+ws1 = xlTemplate.create_sheet("Balance Sheet")
+ws1['A4'] = "Balance Sheet"
+xlTemplate.save(ftotal)
+
+print("Operation Finished")
