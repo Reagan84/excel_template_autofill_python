16ea63fbdd153ba936620a1e999e7773cbe19116
cnel7 <carzonn216@gmail.com>
2025-08-13 20:43:10 -0600

    fleshed out basic of config feature

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 # TODO:
-# have program open, from a file path, excel
+# add flags --> allow user to make changes to config file
+# turn prompt below into cmd line argument
+# have user able to configure different json for template configs
+# build a copy of an excel sheet via template config
+
+import json
 from openpyxl import Workbook, load_workbook
 
-wb = Workbook()
+# path set by user that directs program to excel file location
+userPath = "routeToExcelDirectory"
 
-fpath = "../../../Desktop/"
+# add error catching? -- loads config file for use
+with open('config.json', 'r') as configFile:
+    configData = json.load(configFile)
 
+fpath = configData[userPath]
+
+# test (not permanant) -- needs error catching
 print("Provided me with a file")
 fname = input()
 ftotal = fpath + fname
 xlTemplate = load_workbook(filename = ftotal)
 
-
+# test --> to be changed
 ws1 = xlTemplate.create_sheet("Balance Sheet")
 ws1['A4'] = "Balance Sheet"
 xlTemplate.save(ftotal)
 
+# finising message
 print("Operation Finished")
