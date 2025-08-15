b86545ceba001b2d08e167beacea641c7c54357c
cnel7 <carzonn216@gmail.com>
2025-08-14 19:49:39 -0600

    prints financial statement data from 10q and 10k

--- finStatOb.py
+++ finStatOb.py
@@ -1,10 +1,55 @@
+# TODO:
+# intake an html file defined as fileToParsePath
+# parse through file to find financial information
+# save this information to an object
+
 from bs4 import BeautifulSoup
+import csv
 
-# apple 10q
-fileToParsePath = "appl.htm"
+def main():
+    # apple 10q --> aapl-20250628
+    fileToParsePath = "appl.htm"
 
-# open the file
-fileToParse = open(fileToParsePath)
+    # open the file
+    fileToParse = open(fileToParsePath)
 
-# print out contents thereof
-print(BeautifulSoup(fileToParse, "html.parser"))
+    # print out contents thereof
+    soup = (BeautifulSoup(fileToParse, "html.parser"))
+    
+    tableTitle = soup.find_all(style = "margin-top:9pt;text-align:center")
+    for titles in tableTitle:
+        titleTable = titles.next_sibling.next_sibling.next_sibling
+        finSheet = titleTable.find("table")
+        if finSheet != None:
+            print(titles.string)
+            tableRow = finSheet.find_all("tr")
+            for row in tableRow:
+                csvRow = ""
+                tableData = row.find_all("td")
+                for data in tableData:
+                    for dataString in data.strings:
+                        csvRow += dataString
+                    csvRow += ","
+                print(csvRow)
+            print()
+
+
+
+"""
+    tableInfo = soup.find_all("table")
+    for table in tableInfo:
+        tableRow = table.find_all("tr")
+        for row in tableRow:
+            csvRow = ""
+            tableData = row.find_all("td")
+            for data in tableData:
+                for dataString in data.strings:
+                    csvRow += dataString
+                csvRow += ","
+            print(csvRow)
+        print()
+        print()
+        print("end of table *************")
+"""
+
+main()
